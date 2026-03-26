63131ec4ab805c56ebf2936170e1168dd1964322
Thomas Tanaka <thomas.tanaka@oracle.com>
2026-03-26 12:49:37 -0700

    sync release/2.3 catalog to main  (#329)

--- generate.sh
+++ generate.sh
@@ -12,6 +12,49 @@ if [ -z "$REGISTRY" ]; then
 	REGISTRY=container-registry.oracle.com
 fi
 
+preserve_values_yaml_before_yq() {
+	awk '
+		/^[[:space:]]*$/ {
+			print "#__GENERATE_SH_BLANK__"
+			next
+		}
+		/^[[:space:]]*#/ {
+			print "#" $0
+			next
+		}
+		{
+			print
+		}
+	' values.yaml > values.yaml.preserved
+	mv values.yaml.preserved values.yaml
+}
+
+restore_values_yaml_after_yq() {
+	awk '
+		/^[[:space:]]*#/ {
+			line = $0
+			sub(/^[[:space:]]*#/, "", line)
+			if (line == "__GENERATE_SH_BLANK__") {
+				print ""
+			} else {
+				print line
+			}
+			next
+		}
+		{
+			print
+		}
+	' values.yaml > values.yaml.restored
+	mv values.yaml.restored values.yaml
+}
+
+update_values_yaml_with_yq() {
+	EXPR="$1"
+	preserve_values_yaml_before_yq
+	yq -i "$EXPR" values.yaml
+	restore_values_yaml_after_yq
+}
+
 set -x
 
 REPO_URL=$(yq .repo "$TEMPLATE_FILE")
@@ -42,11 +85,15 @@ yq -i ".icon = \"icons/$ICON\"" Chart.yaml
 yq -i ".kubeVersion = \"$KUBE_VERSION\"" Chart.yaml
 yq -i ".name = \"$APP\"" Chart.yaml
 
-yq -r -0 '.extraChartYqs[]' "$TEMPLATE_FILE" | xargs -0 -I{} yq -i {} Chart.yaml
+if yq -e '.extraChartYqs' "$TEMPLATE_FILE" > /dev/null 2>&1; then
+	yq -r -0 '.extraChartYqs[]' "$TEMPLATE_FILE" | xargs -0 -I{} yq -i {} Chart.yaml
+fi
 
-TRANSFORMS=$(yq -r '.transforms | keys | .[]' "$TEMPLATE_FILE")
+TRANSFORMS=$(yq -r '(.transforms // {}) | keys | .[]' "$TEMPLATE_FILE")
 for xform in $TRANSFORMS; do
-	yq -r -0 ".transforms.\"$xform\".extraImages | .[]" "$TEMPLATE_FILE" | xargs -0 -I{} sed -i "1s;^;# extra-image: {}\n;" templates/$xform
+	if yq -e ".transforms.\"$xform\".extraImages" "$TEMPLATE_FILE" > /dev/null 2>&1; then
+		yq -r -0 ".transforms.\"$xform\".extraImages | .[]" "$TEMPLATE_FILE" | xargs -0 -I{} sed -i "1s;^;# extra-image: {}\n;" templates/$xform
+	fi
 
 	TEXT=$(yq -re ".transforms.\"$xform\".prepend" "$TEMPLATE_FILE" 2>/dev/null)
 	if [ "$?" != "0" ]; then
@@ -57,20 +104,39 @@ for xform in $TRANSFORMS; do
 done
 
 yq '.values' "$TEMPLATE_FILE" > vals.tmp
-yq -i '. *= load("vals.tmp")' values.yaml
+update_values_yaml_with_yq '. *= load("vals.tmp")'
 rm vals.tmp
 
-NUM_LATEST_IMAGES=$(yq '.latestImages | length' "$TEMPLATE_FILE")
-NUM_LATEST_IMAGES=$((NUM_LATEST_IMAGES-1))
-for i in $(seq 0 $NUM_LATEST_IMAGES); do
-	REG_PATH=$(yq ".latestImages[$i].name" "$TEMPLATE_FILE")
-	REG=$(yq "$REG_PATH" values.yaml)
-	IMAGE="$REGISTRY/$REG"
-	LATEST=$(skopeo list-tags "docker://$IMAGE" | yq .Tags[] | grep -v -e '-amd64' | grep -v -e '-arm64' | sort -V | tail -1)
-	echo "latest for $IMAGE: $LATEST"
-	TAG_PATH=$(yq ".latestImages[$i].tag" "$TEMPLATE_FILE")
-	yq -i "$TAG_PATH = \"$LATEST\"" values.yaml
-done
+process_image_tags() {
+	LIST_NAME="$1"
+	TAG_MODE="$2"
+	NUM_IMAGES=$(yq ".${LIST_NAME} | length" "$TEMPLATE_FILE")
+	NUM_IMAGES=$((NUM_IMAGES-1))
+
+	for i in $(seq 0 $NUM_IMAGES); do
+		REG_PATH=$(yq ".${LIST_NAME}[$i].name" "$TEMPLATE_FILE")
+		REG=$(yq "$REG_PATH" values.yaml)
+		IMAGE="$REGISTRY/$REG"
+
+		if [ "$TAG_MODE" = "appVersion" ]; then
+			TAG_PREFIX=$(yq -r ".${LIST_NAME}[$i].prefix // \"v\"" "$TEMPLATE_FILE")
+			TAG="${TAG_PREFIX}${APP_VERSION}"
+			echo "using app version for $IMAGE: $TAG"
+		elif [ "$TAG_MODE" = "latest" ]; then
+			TAG=$(skopeo list-tags "docker://$IMAGE" | yq .Tags[] | grep -v -e '-amd64' | grep -v -e '-arm64' | sort -V | tail -1)
+			echo "latest for $IMAGE: $TAG"
+		else
+			echo "Unsupported tag mode \"$TAG_MODE\" for $IMAGE"
+			exit 1
+		fi
+
+		TAG_PATH=$(yq ".${LIST_NAME}[$i].tag" "$TEMPLATE_FILE")
+		update_values_yaml_with_yq "$TAG_PATH = \"$TAG\""
+	done
+}
+
+process_image_tags images appVersion
+process_image_tags latestImages latest
 
 
 popd # APP-APP_VERSION
